a05f2a2897d40e79995d0d4c0dcc40dd73f61270
Sebastian Carlosama <sebastiantabango@gmail.com>
2024-04-24 15:24:46 -0500

    Yaml file

--- reconocimeinto_numeros/settings.py
+++ reconocimeinto_numeros/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
     # '/var/www/static/',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
