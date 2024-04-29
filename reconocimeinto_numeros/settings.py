68aacf10a7b62a298ca441fcd312d15c7f75ed85
Sebastian Carlosama <sebastiantabango@gmail.com>
2024-04-28 20:22:28 -0500

    Finally set up

--- reconocimeinto_numeros/settings.py
+++ reconocimeinto_numeros/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(qvtgc+h4v=yoxt#t=escn-3!!$)rvvlml14(0&_d4ymh=7y%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower == 'true'
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
