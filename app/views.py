4367ecc97ed0d0d1c689a03adb3b92ff5b012bef
Sebastian Carlosama <sebastiantabango@gmail.com>
2024-04-28 22:17:14 -0500

    Show uploaded image

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .form import UploadFileForm
 from .load_model import extraxt_digit_from_image, load_model
+import base64
 #from django.http import HttpResponse
 
 # Create your views here.
@@ -13,7 +14,13 @@ def index(request):
         if formulario.is_valid():
             image_bytes = request.FILES['Image'].file.read()
             digit = extraxt_digit_from_image(image_bytes)
-            return render(request, 'index.html', {'formulario': formulario, 'digit': digit})
+            # Codificar la imagen para ser utilizada en el HTML
+            encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+            return render(request, 'index.html', {
+                'formulario': formulario,
+                'digit': digit,
+                'image_src': 'data:image/jpeg;base64,' + encoded_image
+            })
     else:
         formulario = UploadFileForm()
         return render(request, 'index.html', {'formulario': formulario})
