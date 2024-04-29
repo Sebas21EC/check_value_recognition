9c871b733b6537800e04fdfdf5b40a468ed68793
Sebastian Carlosama <sebastiantabango@gmail.com>
2024-04-29 07:43:15 -0500

    Pictures added

--- reconocimeinto_numeros/settings.py
+++ reconocimeinto_numeros/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(qvtgc+h4v=yoxt#t=escn-3!!$)rvvlml14(0&_d4ymh=7y%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
