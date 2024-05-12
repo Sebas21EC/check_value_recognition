90e54686a58827445fffdab7179ba789364b6adb
Sebastian Carlosama <sebastiantabango@gmail.com>
2024-05-12 12:16:33 -0500

    check digit recognition implementation

--- app/load_model.py
+++ app/load_model.py
@@ -2,30 +2,105 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1' 
 
 from keras.models import load_model
-from PIL import Image
 import numpy as np
-import io
+import cv2
 
-
-model = load_model('app/data/model_Mnist.h5')
+model = load_model('app/data/model1.h5')
 print('Model loaded......................')
 
 
-def extraxt_digit_from_image(image_bytes):
+def filter_contours_by_area(contours,min_area=5000):
+    return [c for c in contours if cv2.contourArea(c) > min_area]
+
+def filter_contours_by_ratio(contours, aspect_ratio_range=(1.5, 5)):
+    filtered_contours = []
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        aspect_ratio = float(w) / h
+        if aspect_ratio_range[0] <= aspect_ratio <= aspect_ratio_range[1]:
+            filtered_contours.append(cnt)
+    return filtered_contours
+
+def extract_check(image):
+    if image is None or image.size == 0:
+        print("Error: The image is empty or None.")
+        return None
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _,thresh =cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    image=thresh
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = filter_contours_by_area(contours)
+    contours = filter_contours_by_ratio(contours)
     
+    if contours:
+        largest_contour = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        check_image = image[y:y+h, x:x+w]
+        check_image = cv2.resize(check_image, (975, 670))
+        return check_image
+    else:
+        return image
+
+def crop_interest_area(check_image):
+    height, width = check_image.shape
+    left = max(50, width - 320)
+    top = int(height * 0.20)
+    right = width - 100
+    bottom = int(height * 0.28)
+    cropped_image = check_image[top:bottom, left:right]
+    return cropped_image
+
+def invert_image(image):
+    return cv2.bitwise_not(image)
+
+def detect_and_crop_digit(image):
+    if len(image.shape) == 3:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = image
     
-    # Convertir la imagen a escala de grises
-    image = Image.open(io.BytesIO(image_bytes)).convert('L')
-    # Cambiar el tamaño de la imagen
-    image = image.resize((28, 28))
-    # Convertir la imagen a un arreglo de numpy
-    image = np.array(image)
-    # Normalizar la imagen
-    image = image / 255
-    # Cambiar la forma de la imagen
-    image = image.reshape(1, 28, 28, 1)
-    # Realizar la predicción
-    prediction = model.predict([image])
-    # Obtener el dígito predicho
-    digit = np.argmax(prediction)
-    return digit
+
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = filter_contours_by_area(contours, min_area=5)  
+    contours = filter_contours_by_ratio(contours, aspect_ratio_range=(0.1, 1))  
+    contours.sort(key=lambda cnt: cv2.boundingRect(cnt)[0])
+
+    cropped_digits = []
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        margin = 5 
+        digit = image[max(y-margin, 0):y+h+margin, max(x-margin, 0):x+w+margin]
+        cropped_digits.append(digit)
+        
+    return cropped_digits
+
+
+def extract_digit_from_image(cropped_image):
+    digits = []
+    print("len(cropped_image): ", len(cropped_image))
+    for i, digit in enumerate(cropped_image):
+        if len(digit.shape) == 3:
+            digit = cv2.cvtColor(digit, cv2.COLOR_BGR2GRAY)
+        
+        digit = invert_image(digit)
+        digit_resized = cv2.resize(digit, (28, 28))
+        digit_smoothed = cv2.GaussianBlur(digit_resized, (5, 5), 0)
+        if len(digit_smoothed.shape) == 2:
+            digit_smoothed = cv2.cvtColor(digit_smoothed, cv2.COLOR_GRAY2BGR)
+        image_yuv = cv2.cvtColor(digit_smoothed, cv2.COLOR_BGR2YUV)
+        image_yuv[:,:,0] = cv2.equalizeHist(image_yuv[:,:,0])
+        digit_contrast = cv2.cvtColor(image_yuv, cv2.COLOR_YUV2BGR)
+        digit_gray = cv2.cvtColor(digit_contrast, cv2.COLOR_BGR2GRAY)
+        digit_normalized = digit_gray / 255
+        digit_reshaped = digit_normalized.reshape(1, 28, 28, 1)
+        prediction = model.predict([digit_reshaped])
+        predicted_digit = np.argmax(prediction)
+        digits.append(predicted_digit)
+
+    return digits
+
+
--- app/views.py
+++ app/views.py
@@ -1,24 +1,43 @@
 from django.shortcuts import render
 from .form import UploadFileForm
-from .load_model import extraxt_digit_from_image, load_model
+from .load_model import extract_check, crop_interest_area, extract_digit_from_image,detect_and_crop_digit
 import base64
-#from django.http import HttpResponse
+import cv2
+import numpy as np
 
-# Create your views here.
 def index(request):
-
     if request.method == 'POST':
         formulario = UploadFileForm(request.POST, request.FILES)
         if formulario.is_valid():
-            image_bytes = request.FILES['Image'].file.read()
-            digit = extraxt_digit_from_image(image_bytes)
-            # Codificar la imagen para ser utilizada en el HTML
-            encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+            image_bytes = request.FILES['Image'].read()
+            nparr = np.frombuffer(image_bytes, np.uint8) 
+            image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+            if image is None or image.size == 0:
+                print("Error: The image failed to load or is empty.")
+                return render(request, 'index.html', {'formulario': formulario, 'error': 'Failed to load image.'})
+
+            image_check = extract_check(image)
+            cropped_image = crop_interest_area(image_check)
+            cropped_digit = detect_and_crop_digit(cropped_image)
+            digits = extract_digit_from_image(cropped_digit)
+            number_check = "".join(map(str, digits))
+
+            image_check = cv2.cvtColor(image_check, cv2.COLOR_GRAY2BGR)
+            _, encoded_image = cv2.imencode('.png', image_check)
+            image_check = base64.b64encode(encoded_image).decode('utf-8')
+
+            image_interest_area = cv2.cvtColor(cropped_image, cv2.COLOR_GRAY2BGR)
+            _, encoded_image = cv2.imencode('.png', image_interest_area)
+            image_interest_area = base64.b64encode(encoded_image).decode('utf-8')
+
             return render(request, 'index.html', {
-                'formulario': formulario,
-                'digit': digit,
-                'image_src': 'data:image/jpeg;base64,' + encoded_image
-            })
+    'formulario': formulario,
+    'number_check': number_check,
+    'image_check': 'data:image/png;base64,' + image_check,
+    'image_interest_area': 'data:image/png;base64,' + image_interest_area,
+})
+
     else:
         formulario = UploadFileForm()
         return render(request, 'index.html', {'formulario': formulario})
